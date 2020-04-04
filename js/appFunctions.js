520234fabf77f3ef620648771205744332b6a8de
Lyuda.Pik <milasorokina96@gmail.com>
2020-04-04 06:03:35 +0300

    rewritten modal cart for 2 variant by js

--- js/appFunctions.js
+++ js/appFunctions.js
@@ -163,16 +163,104 @@ let getIdFromStorage = (data) => {
 }
 
 //*   MODAL CART   *//
+function creatModalCart() {
+    const nav = document.querySelector('.navigation');
+    const modalOrder = document.createElement('section');
+    modalOrder.classList.add('modal-order');
+    modalOrder.setAttribute('id', 'modalOrder');
+    nav.appendChild(modalOrder);
+    const modalOrderBody = document.createElement('div');
+    modalOrderBody.classList.add('modal-order-body');
+    modalOrder.appendChild(modalOrderBody);
+    creatButtonOrderClose(modalOrderBody);
+    creatOrderHeader(modalOrderBody);
+    creatOrderContent(modalOrderBody);
+    creatOrderFooter(modalOrderBody);
+}
+
+function creatButtonOrderClose(modalOrderBody) {
+    const buttonOrderClose = document.createElement('button');
+    buttonOrderClose.setAttribute('type', 'button');
+    buttonOrderClose.classList.add('modal-order-close');
+    buttonOrderClose.setAttribute('id', 'modalOrderClose');
+    buttonOrderClose.innerText = 'x';
+    modalOrderBody.appendChild(buttonOrderClose);
+    closeOrderCart();
+}
+
+function creatOrderHeader(modalOrderBody) {
+    const modalOrderHeader = document.createElement('div');
+    modalOrderHeader.classList.add('modal-order-header');
+    modalOrderBody.appendChild(modalOrderHeader);
+    const modalOrderTitle = document.createElement('div');
+    modalOrderTitle.classList.add('modal-order-title');
+    modalOrderHeader.appendChild(modalOrderTitle);
+    if (cart.length > 0) {
+        modalOrderTitle.innerText = 'Your order';
+    } else {
+        modalOrderHeader.style.textAlign = 'center';
+        modalOrderTitle.innerText = 'There are no products in the cart.';
+    }
+}
+
+function creatOrderContent(modalOrderBody) {
+    const modalOrderContent = document.createElement('div');
+    modalOrderContent.classList.add('modal-order-content');
+    modalOrderBody.appendChild(modalOrderContent);
+    const modalOrderItems = document.createElement('div');
+    modalOrderItems.classList.add('modal-order-items');
+    modalOrderContent.appendChild(modalOrderItems);
+    if (cart.length > 0) {
+        //* CREAT SELECTED ITEMS *//
+    } else {
+        modalOrderItems.innerText = 'You have a good taste. And we have a lot of interesting and necessary items.';
+        modalOrderContent.style.textAlign = 'center';
+    }
+}
+
+function creatOrderFooter(modalOrderBody) {
+    const modalOrderFooter = document.createElement('div');
+    modalOrderFooter.classList.add('modal-order-footer');
+    modalOrderBody.appendChild(modalOrderFooter);
+    if (cart.length > 0) {
+        const modalOrderTotal = document.createElement('div');
+        modalOrderTotal.classList.add('modal-order-total');
+        modalOrderTotal.innerText = 'Total:';
+        modalOrderFooter.appendChild(modalOrderTotal);
+
+        const buttonOrderContinue = document.createElement('button');
+        buttonOrderContinue.setAttribute('type', 'button');
+        buttonOrderContinue.classList.add('modal-order-button', 'modal-order-continue');
+        buttonOrderContinue.setAttribute('id', 'modalOrderContinue');
+        buttonOrderContinue.innerText = 'Continue shopping';
+        modalOrderFooter.appendChild(buttonOrderContinue);
+
+        const buttonOrderCheckout = document.createElement('button');
+        buttonOrderCheckout.setAttribute('type', 'button');
+        buttonOrderCheckout.classList.add('modal-order-button');
+        buttonOrderCheckout.setAttribute('id', 'modalOrderCheckout');
+        buttonOrderCheckout.innerText = 'Checkout';
+        modalOrderFooter.appendChild(buttonOrderCheckout);
+        continueOrder();
+        openOrder();
+    } else {
+        const buttonOrderShopping = document.createElement('button');
+        buttonOrderShopping.setAttribute('type', 'button');
+        buttonOrderShopping.classList.add('modal-order-button');
+        buttonOrderShopping.setAttribute('id', 'modalOrderShopping');
+        buttonOrderShopping.innerText = 'Start shopping';
+        modalOrderFooter.appendChild(buttonOrderShopping);
+        modalOrderFooter.style.textAlign = 'center';
+        startShopping();
+    }
+}
 
 function openModalOrder() {
     let cartButton = document.getElementById('cartButton');
     let modalOrder = document.getElementById('modalOrder');
     cartButton.addEventListener('click', () => {
-        modalOrder.style.display = 'flex';
         document.body.style.overflow = 'hidden';
-        closeOrderCart();
-        continueOrder();
-        openOrder();
+        creatModalCart();
     })
 }
 
@@ -180,7 +268,7 @@ function closeOrderCart() {
     let modalOrderClose = document.getElementById('modalOrderClose');
     let modalOrder = document.getElementById('modalOrder');
     modalOrderClose.addEventListener('click', () => {
-        modalOrder.style.display = 'none';
+        modalOrder.remove();
     })
 }
 
@@ -188,7 +276,7 @@ function continueOrder() {
     let modalOrderContinue = document.getElementById('modalOrderContinue');
     let modalOrder = document.getElementById('modalOrder');
     modalOrderContinue.addEventListener('click', () => {
-        modalOrder.style.display = 'none';
+        modalOrder.remove();
     })
 }
 
@@ -198,6 +286,16 @@ function openOrder() {
     modalOrderCheckout.addEventListener('click', () => {
         modalOrderCheckout.classList.add('another-page');
         modalOrderCheckout.setAttribute('data-page', '5');
-        modalOrder.style.display = 'none';
+        modalOrder.remove();
+    })
+}
+
+function startShopping() {
+    let cartButtonShopping = document.getElementById('modalOrderShopping');
+    let modalOrder = document.getElementById('modalOrder');
+    cartButtonShopping.addEventListener('click', () => {
+        cartButtonShopping.classList.add('another-page');
+        cartButtonShopping.setAttribute('data-page', '4');
+        modalOrder.remove();
     })
 }
